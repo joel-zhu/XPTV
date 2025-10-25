11231f4391fc2b27bdbc8b35dbd1170b0a649507
joel-zhu <junyuzhu007@gmail.com>
2025-10-25 15:15:39 +0800

    Update avdb.js

--- js/avdb.js
+++ js/avdb.js
@@ -4,7 +4,7 @@ const config = argsify($config_str)
 let appConfig = {
     ver: 1,
     title: 'avdb',
-    site: 'https://avdbapi.com',
+    site: 'https://avdbapi.com/api.php/provide/vod',
 }
 
 async function getConfig() {
